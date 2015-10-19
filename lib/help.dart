fb9f7f5db207bfbcf138abebf16d649bcb34c39a
Kenneth Endfinger <kaendfinger@gmail.com>
2015-10-19 18:14:23 -0400

    Update Help

--- lib/help.dart
+++ lib/help.dart
@@ -3,9 +3,18 @@ library dsa.tool.help;
 const String HELP_COMMAND = """\
 DSA Tool Help:
 
+  Fetch a DSLink:
+    dsa get dslink-java-etsdb
+
   List Java DSLinks:
     dsa link list -t java
 
   List Dart DSLinks:
     dsa link list -t dart
+
+  Run a Batch Task File:
+    dsa batch run ~/Tasks/my_task.yaml
+
+  Clone all Java DSLinks:
+    dsa link list -t java | bash
 """;
